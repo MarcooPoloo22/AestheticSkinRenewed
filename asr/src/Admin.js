d43578a6a9c7baeb950becc2aaa7c1d633cbd825
Gab Herrera <gabriellogerald.herrera.cics@ust.edu.ph>
2025-01-27 14:09:43 +0800

    Updated foldering for components and css

---
+++ asr/src/Admin.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AdminLogin from './components/Admin/AdminLogin';
+
+function Admin() {
+  return (
+    <div className="App">
+      <AdminLogin />
+    </div>
+  );
+}
+
+export default Admin;
