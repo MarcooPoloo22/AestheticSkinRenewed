e4d16abcd20b3a1c3042abe14f797eefa2cb1f85
Blaze816 <ejpangilinan816.ejp@gmail.com>
2025-02-06 14:18:20 +0800

    Add minimum height to ServiceCard description area

--- src/components/Customers/ServiceCard.jsx
+++ src/components/Customers/ServiceCard.jsx
@@ -7,7 +7,7 @@ const ServiceCard = ({ title, description, image, updated }) => {
                 <div className="col-md-4">
                     <img src={image} className="img-fluid rounded-start w-100 h-100" alt={title} />
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-8" style={{ minHeight: '150px' }}>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
